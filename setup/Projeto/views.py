a287718eb39289eb0a79d449c5828e9b22380ef5
Nilson <nilson.pagneto@gmail.com>
2024-10-01 20:17:48 -0300

    Resolução Questão5

--- setup/Projeto/views.py
+++ setup/Projeto/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Projeto")
+    return render(request, './home.html')
 
-# Create your views here.
+
